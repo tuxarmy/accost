33208a4acad2ceafb45d87b5c2d7a3e219de4c62
tuxarmy <ambarrubiyanto@gmail.com>
2015-07-16 10:33:54 +0700

    login & register page

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['admin'] = 'admin/dashboard';
 
+$route['(:any)'] = 'pages/$1';
 $route['default_controller'] = 'pages';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -5,4 +5,12 @@ class Pages extends Public_Controller {
 	public function index(){
 		$this->render('public/homepage');
 	}
+
+	public function login(){
+		$this->render('public/login');
+	}
+
+	public function register(){
+		$this->render('public/register');
+	}
 }
